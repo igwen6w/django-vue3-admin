048a121dc9f6e3e690b3080d71f434366603e2ce
igwen6w <igwen6w@gmail.com>
2025-09-14 12:26:31 +0800

    feat(category): add tree action to CategoryViewSet for hierarchical category structure retrieval

--- backend/work_order/views/category.py
+++ backend/work_order/views/category.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 from work_order.models import Category
 from utils.serializers import CustomModelSerializer
 from utils.custom_model_viewSet import CustomModelViewSet
+from rest_framework import status
 from django_filters import rest_framework as filters
+from rest_framework.decorators import action
 
 
 class CategorySerializer(CustomModelSerializer):
@@ -31,6 +34,34 @@ class CategoryViewSet(CustomModelViewSet):
     search_fields = ['name']  # 根据实际字段调整
     ordering_fields = ['create_time', 'id']
     ordering = ['-create_time']
+
+    @action(detail=False, methods=['get'])
+    def tree(self, request):
+        """获取工单分类树形结构"""
+        all_categories = Category.objects.all().order_by('-id')
+        category_dict = {}
+        children_map = defaultdict(list)
+        for category in all_categories:
+            item = {
+                'id': category.id,
+                'parent': category.parent_id,
+                'name': category.name,
+                'description': category.description,
+                'children': [],
+            }
+            category_dict[category.id] = item
+            if category.parent_id:
+                children_map[category.parent_id].append(item)
+            
+            for category_id, category in category_dict.items():
+                category['children'] = children_map.get(category_id, [])
+                
+        tree = [category for category in category_dict.values() if category['parent'] is None]
+        return self._build_response(
+            data=tree,
+            message="ok",
+            status=status.HTTP_200_OK,
+        )
 
 # 移入urls中
 # router.register(r'category', views.CategoryViewSet)
